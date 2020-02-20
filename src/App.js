ad4374149b038a915bbc15e23bec65a0be5d0ca1
Francisco Bouza <fbouza@uni.ort.edu.uy>
2020-02-19 22:49:31 -0300

    Routeo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from "react";
-// import ManejoTareas from "./components/ManejoTareas";
-import Login from "./components/Login";
-import "./App.scss";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import ManejoTareas from "./components/ManejoTareas";
+import Login from './components/Login';
+import PageNotFound from './components/PageNotFound';
+import './App.scss';
 
 class App extends Component {
-  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>To-Do List!</p>
         </header>
-        {/* <ManejoTareas /> */}
-        <Login />
+        <Switch>
+          <Route path="/tareas" component={ManejoTareas} exact/>
+          <Route path="/login" component={Login} exact/>
+          <Route component={PageNotFound}/>
+        </Switch>
       </div>
     );
   }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
 import '../styles/login.scss';
 
-const Login = () => {
+const Login = ({ history, pochoclo }) => {
+
+  // useEffect(() => {
+    // bla bla
+  // },[]);
+
+  const [email, setEmail] = useState('fran@cisco.com');
+  const [password, setPassword] = useState('francisco');
+  const [errorMsg, setErrorMsg] = useState('');
+
+  // const handleEmailChange = ({ target: { value } }) => {
+  //   setEmail(value);
+  // }
+
+  // const handlePasswordChange = ({ target: { value } }) => {
+  //   setPassword(value);
+  // }
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    fetch('http://tiendaonline2020.herokuapp.com/api/user/login',{
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+        .then(res => res.json())
+        .then(res => {
+          console.log("Éxito!",res);
+          if(res.status && res.status !== 200)
+          {
+            setErrorMsg("No existe un usuario con dicha contraseña. Intente nuevamente.");
+          } else {
+            history.push("/tareas");
+          }
+        })
+        .catch(err => console.log("Error!",err));
   }
 
   return (
     <div className="login">
       <form onSubmit={handleSubmit} className="login-form">
-        <label htmlFor="username">Nombre de usuario: </label>
-        <input type="email" name="username" required/>
+        <label htmlFor="email">Nombre de usuario: </label>
+        <input type="email" name="email" value={email} onChange={({ target: { value } }) => setEmail(value)} required/>
         <label htmlFor="password">Contraseña: </label>
-        <input type="password" name="password" required/>
+        <input type="password" name="password" value={password} onChange={({ target: { value } }) => setPassword(value)} required/>
         <input type="submit" value="Iniciar Sesión"/>
+        { errorMsg &&
+          <h2 className="error">{errorMsg}</h2>
+        }
       </form>
     </div>
   );
@@ -24,4 +66,4 @@ Login.propTypes = {
 
 };
 
-export default Login;
+export default withRouter(Login);
